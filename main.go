db50692f1bc6cd400db058fd2315abfc8a1c3601
Sreinumder <rojgosai02@gmail.com>
2026-04-23 21:05:49 +0545

    args handling

--- main.go
+++ main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	"rojgosai.com.np/golf/pkg/ls"
 	"rojgosai.com.np/golf/pkg/pwd"
 )
 
-func main() {
-	if dir, err := pwd.Pwd(); err == nil {
-		fmt.Print("Current pwd:", dir)
-	} else {
-		print(err)
+func parseFlags(args []string) (map[string]int, []string) {
+	flags := make(map[string]int)
+	var paths []string
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "--") && len(arg) > 2 {
+			flag := arg[2:]
+			flags[flag]++
+		} else if strings.HasPrefix(arg, "-") && len(arg) > 1 {
+			for _, ch := range arg[1:] {
+				flags[string(ch)]++
+			}
+		} else {
+			paths = append(paths, arg)
+		}
 	}
+	return flags, paths
+}
 
+func main() {
+	args := os.Args
+
+	switch args[1] {
+	case "pwd":
+		fmt.Println(pwd.Pwd())
+	case "ls":
+		// Parse flags and paths
+		flags, paths := parseFlags(args[2:])
+		if len(paths) > 0 {
+			for _, path := range paths {
+				fmt.Println("\n" + path + ":")
+				ls.Ls(path, flags)
+			}
+		} else {
+			fmt.Println(".:")
+			ls.Ls(".", flags)
+		}
+	}
 }
